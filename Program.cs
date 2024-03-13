3663891e6f1730f4d3770acac044b252b40c3dc1
danellopes <daniel.nunes.meira@outlook.com>
2024-03-13 08:53:47 -0300

    Finalized copy constructor exercise.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿namespace C_CopyConstructors;
+
+class Program
+{
+    public class Person
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            if (names == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(names));
+            }
+            if (address == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(address));
+            }
+
+            Names = names;
+            Address = address;
+        }
+
+        public Person(Person other)
+        {
+            Names = other.Names;
+            Address = new Address(other.Address);
+        }
+
+        public override string ToString()
+        {
+            return $"Names: {string.Join(" ", Names)}, Address: {Address}";
+        }
+    }
+
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            if (streetName == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(streetName));
+            }
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public Address(Address other)
+        {
+            StreetName = other.StreetName;
+            HouseNumber = other.HouseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"StreetName: {StreetName}, HouseNumber: {HouseNumber}";
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+        var jane = new Person(john);
+        jane.Names = new[] { "Jane", "Smith" };
+        jane.Address.HouseNumber = 321;
+
+        System.Console.WriteLine(john);
+        System.Console.WriteLine(jane);
+    }
+}
